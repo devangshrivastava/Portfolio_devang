5c1e12194855aa4aae2b4d74def05243a369e27f
devangshrivastava <devangshrivastava27@gmail.com>
2024-04-16 07:15:22 +0530

    minor changes

--- lib/data.ts
+++ lib/data.ts
@@ -3,7 +3,7 @@ import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/corpcomment.png";
-import pyqimg from "@/public/pyq.png";
+import pyqimg from "@/public/pyq_img.png";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
